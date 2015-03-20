86dde905321a59f2a29794e63bc7d30ae484de75
mahira galal <mahiragalal.mg@gmail.com>
2015-03-20 23:07:30 +0200

    i made my account

--- financeweb/financeweb/urls.py
+++ financeweb/financeweb/urls.py
@@ -1,14 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from financeweb.views import home, abc, home2
+from sample.views import home, home2, myaccount, signup, home3, editprofile
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'financeweb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', home, name='home'),
+    url(r'^homepage$', home, name='home'),
     url(r'^homepage2$', home2, name='home2'),
-	url(r'^signup$', abc, name='abc'),
+    url(r'^homepage3/(?P<u_id>\d+)$', home3, name='home3'),
+	url(r'^signup$', signup, name='signup'),
+	url(r'^myaccount/(?P<u_id>\d+)$', myaccount, name='myaccount'),
+	url(r'^editprofile/(?P<u_id>\d+)$', editprofile, name='editprofile'),
+
 )
