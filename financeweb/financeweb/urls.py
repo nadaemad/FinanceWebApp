e1358b43c88c3a88caa0fb8f220f1c0602f5142f
Nada Emad <nada.emad8@gmail.com>
2015-03-20 23:28:29 +0200

    I made sign in and modified a few things accordingly

--- financeweb/financeweb/urls.py
+++ financeweb/financeweb/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from financeweb.views import home, abc, home2
+from financeweb.views import home, abc, home2, login_user
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,7 +8,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', home, name='home'),
+    url(r'home^$', home, name='home'),
     url(r'^homepage2$', home2, name='home2'),
 	url(r'^signup$', abc, name='abc'),
+	url(r'^signin', login_user, name='signin'),
 )
